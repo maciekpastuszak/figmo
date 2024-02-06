1747e4d82e3382d3eabb3a0c1eb6714d5dd50705
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-02-06 22:47:10 +0100

    update liveblocks client config

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,9 +2,8 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
   
 const client = createClient({
-  // publicApiKey: "",
-  // authEndpoint: "/api/auth",
-  // throttle: 100,
+  publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY!,
+
 });
 
 // Presence represents the properties that exist on every user in the Room
