73883a0c13b5c01fb846aabd98b0ff1b09d0f6bf
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-02-08 09:42:22 +0100

    center main text

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { Room } from "./Room";
 
 export default function Page() {
   return (
+    <div className="h-[100vh] w-full flex justify-center items-center text-center">
       <h1 className="text-5xl text-white">Figmo</h1>
+    </div>
   );
 }
