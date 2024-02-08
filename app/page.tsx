3c42ac2223b9b4659bd3ec705348b705db379f70
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-02-08 09:43:46 +0100

    add Live component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Room } from "./Room";
 export default function Page() {
   return (
     <div className="h-[100vh] w-full flex justify-center items-center text-center">
-      <h1 className="text-5xl text-white">Figmo</h1>
+      <h1 className="text-2xl text-white">Figmo</h1>
     </div>
   );
 }
