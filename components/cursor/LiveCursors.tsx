fd96cca4a06fadc54586b602cc387b6b797949de
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-02-09 09:23:42 +0100

    map through cursors in LiveCursors

--- components/cursor/LiveCursors.tsx
+++ components/cursor/LiveCursors.tsx
@@ -1,10 +1,17 @@
 import { LiveCursorProps } from '@/types/type'
 import React from 'react'
+import Cursor from './Cursor';
 
 const LiveCursors = ({others}: LiveCursorProps) => {
-  return (
-    <div>LiveCursors</div>
-  )
+  others.map(({connectionId, presence}) => {
+    if(!presence) return null;
+
+    return(
+      <Cursor 
+        key={connectionId}
+      />
+    )
+  })
 }
 
 export default LiveCursors
