51e99b2ef77c40a65e5c0ab1aafd781f4dfefb51
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-02-11 12:51:41 +0100

    add input to the cursor in CursorChat

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -1,10 +1,10 @@
 import CursorSVG from '@/public/assets/CursorSVG'
 import { CursorChatProps, CursorMode } from '@/types/type'
+// {cursorState.mode === CursorMode.Chat && (      )}
 
 const CursorChat = ({ cursor, cursorState, setCursorState, updateMyPresence }: CursorChatProps) => {
   return (
     <div className="absolute top-0 left-0" style={{ transform: `translatex(${cursor.x}px) translateY(${cursor.y}px)`}}>
-      {cursorState.mode === CursorMode.Chat && (
         <>
           <CursorSVG color="#000" />
 
@@ -12,9 +12,13 @@ const CursorChat = ({ cursor, cursorState, setCursorState, updateMyPresence }: C
             {cursorState.previousMessage && (
               <div>{cursorState.previousMessage}</div>
             )}
+
+            <input
+              className="z-10 w-60 border-none bg-transparent text-white placeholder-blue-200 outline-none"
+              autoFocus={true}
+            />
           </div>
         </>
-      )}
     </div>
   )
 }
