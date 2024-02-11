f61b7a111fc04ce3ecd9562f180b487371428764
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-02-11 12:45:55 +0100

    add cursorSVG and previous message to CursorChat

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -1,11 +1,19 @@
+import CursorSVG from '@/public/assets/CursorSVG'
 import { CursorChatProps, CursorMode } from '@/types/type'
 
 const CursorChat = ({ cursor, cursorState, setCursorState, updateMyPresence }: CursorChatProps) => {
   return (
     <div className="absolute top-0 left-0" style={{ transform: `translatex(${cursor.x}px) translateY(${cursor.y}px)`}}>
       {cursorState.mode === CursorMode.Chat && (
-      <>
-      </>
+        <>
+          <CursorSVG color="#000" />
+
+          <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
+            {cursorState.previousMessage && (
+              <div>{cursorState.previousMessage}</div>
+            )}
+          </div>
+        </>
       )}
     </div>
   )
