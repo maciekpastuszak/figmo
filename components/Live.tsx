3bcf059325b042126bca491a2f5b57dda29db022
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-02-14 10:49:51 +0100

    add setReactions helper in Live

--- components/Live.tsx
+++ components/Live.tsx
@@ -91,7 +91,10 @@ const Live = () => {
     }
   }, [updateMyPresence])
   
-
+  const setReactions = useCallback((reaction: string) => {
+    setCursorState({ mode: CursorMode.Reaction, reaction, isPressed: false})
+  }, []);
+  
   return (
     <div
       onPointerMove={handlePointerMove}
@@ -113,9 +116,7 @@ const Live = () => {
 
       {cursorState.mode === CursorMode.ReactionSelector && (
         <ReactionSelector 
-          setReaction={(reaction) => {
-            setReaction(reaction);
-          }}
+          setReaction={setReactions}
         />
       )}
 
