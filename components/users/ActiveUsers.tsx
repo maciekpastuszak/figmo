0f72630dff76f0e42af624e2f9375324711efd9a
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-02-17 21:33:13 +0100

    update Avatar component

--- components/users/ActiveUsers.tsx
+++ components/users/ActiveUsers.tsx
@@ -2,6 +2,7 @@ import { useOthers, useSelf } from "@/liveblocks.config";
 import { Avatar } from "./Avatar";
 
 import styles from './index.module.css';
+import { generateRandomName } from "@/lib/utils";
 
 const ActiveUsers = () => {
     const users = useOthers();
@@ -11,14 +12,13 @@ const ActiveUsers = () => {
     return (
       <main className="flex h-screen w-full select-none place-content-center place-items-center">
         <div className="flex pl-3">
-
         {currentUser && (
             <Avatar name="You" otherStyles="border-[3px] border-primary-green" />
           )}
-          
+
           {users.slice(0, 3).map(({ connectionId, info }) => {
             return (
-              <Avatar key={connectionId} src={info.avatar} name={info.name} />
+              <Avatar key={connectionId} name={generateRandomName()} otherStyles="-ml-3" />
             );
           })}
   
--- components/users/Avatar.tsx
+++ components/users/Avatar.tsx
@@ -3,11 +3,11 @@ import styles from "./Avatar.module.css";
 
 const IMAGE_SIZE = 48;
 
-export function Avatar({ src, name }: { src: string; name: string }) {
+export function Avatar({ name, otherStyles }: { src: string; name: string }) {
   return (
     <div className={styles.avatar} data-tooltip={name}>
       <img
-        src={src}
+        src={`https://liveblocks.io/avatars/avatar-${Math.floor(Math.random() * 30)}.png`}
         height={IMAGE_SIZE}
         width={IMAGE_SIZE}
         className={styles.avatar_picture}
