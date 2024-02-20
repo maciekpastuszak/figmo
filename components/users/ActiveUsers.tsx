1bc2cf67f127c6dcf77ac42a3c3c906dd8ceb28b
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-02-20 22:22:28 +0100

    update ActiveUsers view

--- components/users/ActiveUsers.tsx
+++ components/users/ActiveUsers.tsx
@@ -10,23 +10,21 @@ const ActiveUsers = () => {
     const hasMoreUsers = users.length > 3;
   
     return (
-      <main className="flex w-full select-none place-content-center place-items-center">
+      <div className="flex items-center justify-center gap-1">
         <div className="flex pl-3">
         {currentUser && (
             <Avatar name="You" otherStyles="border-[3px] border-primary-green" />
           )}
 
-          {users.slice(0, 3).map(({ connectionId, info }) => {
+          {users.slice(0, 3).map(({ connectionId }) => {
             return (
               <Avatar key={connectionId} name={generateRandomName()} otherStyles="-ml-3" />
             );
           })}
   
           {hasMoreUsers && <div className={styles.more}>+{users.length - 3}</div>}
-  
-          
         </div>
-      </main>
+      </div>
     );
   }
 
