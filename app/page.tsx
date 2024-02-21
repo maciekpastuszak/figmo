1e3dc4f132f99c9457c4ed7c118bd9dedd8ab291
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-02-21 09:38:09 +0100

    add useRefs in page for the canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 "use client"
 
+import { fabric } from "fabric";
 import Live from "@/components/Live";
 import { Room } from "./Room";
 import Navbar from "@/components/Navbar";
 import LeftSidebar from "@/components/LeftSidebar";
 import RightSidebar from "@/components/RightSidebar";
+import { useRef } from "react";
+import { CustomFabricObject } from "@/types/type";
 
 export default function Page() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const fabricRef = useRef<fabric.Canvas | null>(null);
+
   return (
     <main className="h-screen overflow-hidden">
         <Navbar />
