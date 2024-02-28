dea547d0ec5833d427552b2331106a9d0cd10993
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-02-28 14:33:37 +0100

    bugfix canvas dispose

--- app/page.tsx
+++ app/page.tsx
@@ -128,6 +128,10 @@ export default function Page() {
     window.addEventListener("resize", () => {
       handleResize({ fabricRef })
       })
+
+      return () => {
+        canvas.dispose();
+      }
   }, [])
 
   useEffect(() => {
