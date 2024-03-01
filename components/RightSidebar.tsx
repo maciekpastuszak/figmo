76703f03648fb5815df5485588d080d560e1a69e
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-03-01 10:15:27 +0100

    add handleInputchange method to RightSidebar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -13,12 +13,28 @@ const RightSidebar = ({
   isEditingRef,
   syncShapeInStorage
 }: RightSidebarProps) => {
+  const handleInputchange = (property: string, value: string) => {
+    if(!isEditingRef.current) isEditingRef.current = true;
+
+    setElementAttributes((prev) => ({
+      ...prev, [property]: value
+    }))
+
+    modifyShape({
+      canvas: fabricRef.current as fabric
+    })
+  }
+
   return (
     <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-2-[227px] sticky right-0 h-full max-sm:hidden select-one">
        <h3 className="px-5 pt-4 text-xs uppercase">Design</h3>
        <span className="text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4">Make changes to canvas as you like</span>
 
-       <Dimensions />
+       <Dimensions 
+        width={elementAttributes.width}
+        height={elementAttributes.height}
+        handleInputChange={handleInputChange}
+       />
        <Text />
        <Color />
        <Color />
