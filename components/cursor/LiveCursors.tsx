76aeb7e532a7805663a61ebe6e4914c9dd05d6c3
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2024-03-20 15:30:49 +0100

    bugfix LiveCursor

--- components/cursor/LiveCursors.tsx
+++ components/cursor/LiveCursors.tsx
@@ -15,7 +15,7 @@ const LiveCursors = () => {
         color={COLORS[Number(connectionId) % COLORS.length]}
         x={presence.cursor.x}
         y={presence.cursor.y}
-        message={presence.message}
+        message={presence.message || ''}
       />
     )
   })
