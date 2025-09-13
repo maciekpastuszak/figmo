a75d73d24860ec7238dac76fb84ddb28c6d006fd
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2025-09-13 17:50:58 +0200

    localize text in RightSidebar, Export, and Text components to Silesian

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,10 +1,10 @@
-import React, { useRef } from 'react'
-import Dimensions from './settings/Dimensions'
-import Text from './settings/Text'
-import Color from './settings/Color'
-import Export from './settings/Export'
-import { RightSidebarProps } from '@/types/type'
-import { modifyShape } from '@/lib/shapes'
+import React, { useRef } from "react";
+import Dimensions from "./settings/Dimensions";
+import Text from "./settings/Text";
+import Color from "./settings/Color";
+import Export from "./settings/Export";
+import { RightSidebarProps } from "@/types/type";
+import { modifyShape } from "@/lib/shapes";
 
 const RightSidebar = ({
   elementAttributes,
@@ -12,61 +12,64 @@ const RightSidebar = ({
   fabricRef,
   activeObjectRef,
   isEditingRef,
-  syncShapeInStorage
+  syncShapeInStorage,
 }: RightSidebarProps) => {
   const colorInputRef = useRef(null);
   const strokeInputRef = useRef(null);
 
   const handleInputChange = (property: string, value: string) => {
-    if(!isEditingRef.current) isEditingRef.current = true;
+    if (!isEditingRef.current) isEditingRef.current = true;
 
     setElementAttributes((prev) => ({
-      ...prev, [property]: value
-    }))
+      ...prev,
+      [property]: value,
+    }));
 
     modifyShape({
       canvas: fabricRef.current as fabric.Canvas,
       property,
       value,
       activeObjectRef,
-      syncShapeInStorage
-    })
-  }
+      syncShapeInStorage,
+    });
+  };
 
   return (
     <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-2-[227px] sticky right-0 h-full max-sm:hidden select-one">
-       <h3 className="px-5 pt-4 text-xs uppercase">Design</h3>
-       <span className="text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4">Make changes to canvas as you like</span>
+      <h3 className="px-5 pt-4 text-xs uppercase">Projekt</h3>
+      <span className="text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4">
+        Wkludź zmiany na płōtnie, jak chcesz
+      </span>
 
-       <Dimensions 
+      <Dimensions
         width={elementAttributes.width}
         height={elementAttributes.height}
         handleInputChange={handleInputChange}
         isEditingRef={isEditingRef}
-       />
-       <Text 
+      />
+      <Text
         fontFamily={elementAttributes.fontFamily}
         fontSize={elementAttributes.fontSize}
         fontWeight={elementAttributes.fontWeight}
         handleInputChange={handleInputChange}
-       />
-       <Color 
+      />
+      <Color
         inputRef={colorInputRef}
         attribute={elementAttributes.fill}
-        attributeType='fill'
-        placeholder='color'
+        attributeType="wypołnić"
+        placeholder="farba"
         handleInputChange={handleInputChange}
-       />
-       <Color 
+      />
+      <Color
         inputRef={strokeInputRef}
         attribute={elementAttributes.stroke}
-        attributeType='stroke'
-        placeholder='stroke'
+        attributeType="szlag"
+        placeholder="szlag"
         handleInputChange={handleInputChange}
-       />
-       <Export />
+      />
+      <Export />
     </section>
-  )
-}
+  );
+};
 
-export default RightSidebar
+export default RightSidebar;
