f428fbcc7919748b8e2a21a79f648d69527d0fc7
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2025-09-13 17:54:33 +0200

    localize shape names in LeftSidebar and update shape info in utils to Silesian

--- lib/utils.ts
+++ lib/utils.ts
@@ -51,43 +51,43 @@ export const getShapeInfo = (shapeType: string) => {
     case "rect":
       return {
         icon: "/assets/rectangle.svg",
-        name: "Rectangle",
+        name: "Prostokōnt",
       };
 
     case "circle":
       return {
         icon: "/assets/circle.svg",
-        name: "Circle",
+        name: "Kōłko",
       };
 
     case "triangle":
       return {
         icon: "/assets/triangle.svg",
-        name: "Triangle",
+        name: "Tryjōnt",
       };
 
     case "line":
       return {
         icon: "/assets/line.svg",
-        name: "Line",
+        name: "Linja",
       };
 
     case "i-text":
       return {
         icon: "/assets/text.svg",
-        name: "Text",
+        name: "Tekst",
       };
 
     case "image":
       return {
         icon: "/assets/image.svg",
-        name: "Image",
+        name: "Ôbrŏzek",
       };
 
     case "freeform":
       return {
         icon: "/assets/freeform.svg",
-        name: "Free Drawing",
+        name: "Swobodne Bazgroły",
       };
 
     default:
